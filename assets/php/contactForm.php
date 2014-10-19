8073ae362846886670f25bb0fb2c21624139b314
Jarod Stewart <stewartjarod@gmail.com>
2014-10-18 20:51:44 -0600

    initial relearse

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -1,7 +1,7 @@
 <?php
 
 	// Contact
-	$to = 'web-master72@yandex.ru';
+	$to = 'jarod@jarod.is';
     $subject = 'Subject here...';
 
 	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_message'])){
@@ -9,19 +9,19 @@
     	$from    = $_POST['c_email'];
     	$message = $_POST['c_message'];
 
-		if (mail($to, $subject, $message, $from)) { 
+		if (mail($to, $subject, $message, $from)) {
 			$result = array(
 				'message' => 'Thanks for contacting us!',
 				'sendstatus' => 1
 				);
 			echo json_encode($result);
-		} else { 
+		} else {
 			$result = array(
 				'message' => 'Sorry, something is wrong',
 				'sendstatus' => 1
 				);
 			echo json_encode($result);
-		} 
+		}
 	}
 
 ?>
