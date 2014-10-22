7136ec7de28e14a621b2bbe6b43e459bb9be1210
Jarod Stewart <StewartJarod@users.noreply.github.com>
2014-10-22 14:06:49 -0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,7 +10,21 @@
 	// });
 
 	$(document).ready(function() {
-  		$.adaptiveBackground.run()
+		var defaults      = {
+		  selector:             '[data-adaptive-background="1"]',
+		  parent:               "section",
+		  exclude:              [ 'rgb(0,0,0)', 'rgba(255,255,255)' ],
+		  normalizeTextColor:   false,
+		  normalizedTextColors:  {
+		    light:      "#fff",
+		    dark:       "#000"
+		  },
+		  lumaClasses:  {
+		    light:      "ab-light",
+		    dark:       "ab-dark"
+		  }
+		};
+		$.adaptiveBackground.run(defaults)
 
 		$('body').scrollspy({
 			target: '.navbar-custom',
