13c86f1722d50fb4d31262a9fc0012b31d883a60
Jarod Stewart <StewartJarod@users.noreply.github.com>
2015-04-17 13:02:53 -0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,21 +10,21 @@
 	// });
 
 	$(document).ready(function() {
-		// var defaults      = {
-		//   selector:             '[data-adaptive-background="1"]',
-		//   parent:               "section",
-		//   exclude:              [ 'rgb(0,0,0)', 'rgba(255,255,255)' ],
-		//   normalizeTextColor:   false,
-		//   normalizedTextColors:  {
-		//     light:      "#fff",
-		//     dark:       "#000"
-		//   },
-		//   lumaClasses:  {
-		//     light:      "ab-light",
-		//     dark:       "ab-dark"
-		//   }
-		// };
-		// $.adaptiveBackground.run(defaults);
+		var defaults      = {
+		  selector:             '[data-adaptive-background="1"]',
+		  parent:               "section",
+		  exclude:              [ 'rgb(0,0,0)', 'rgba(255,255,255)' ],
+		  normalizeTextColor:   false,
+		  normalizedTextColors:  {
+		    light:      "#fff",
+		    dark:       "#000"
+		  },
+		  lumaClasses:  {
+		    light:      "ab-light",
+		    dark:       "ab-dark"
+		  }
+		};
+		$.adaptiveBackground.run(defaults);
 
 		$('body').scrollspy({
 			target: '.navbar-custom',
